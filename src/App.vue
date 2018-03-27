1cfd34f5da14687b1d08758f3ede04dbf6c486e1
pig <576618121@qq.com>
2018-03-27 21:18:40 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+  	<!-- Hello World -->
+    <!-- <svg-icon icon-class="user"></svg-icon> -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
